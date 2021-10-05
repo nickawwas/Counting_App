84550d91d32cda203b09e2c0b0333f450fe8e0c1
Nicholas Kawwas <68450354+nickawwas@users.noreply.github.com>
2021-10-05 15:03:17 +0000

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/counting_app/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/counting_app/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.kawwas_ass1", appContext.getPackageName());
+        assertEquals("com.example.counting_app", appContext.getPackageName());
     }
 }
